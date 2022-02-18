1ff346982846dcf4993880fc596b488b3b4ff76b
Федор <radiatorow@gmail.com>
2022-02-18 12:15:08 +0300

    carcass

---
+++ funny_videos/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Video(models.Model):
+    video_name = models.CharField(max_length = 200)
--- video_site/urls.py
+++ video_site/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('funny_videos', include('funny_videos.urls')),
     path('admin/', admin.site.urls),
 ]
