252e307edb71f2ddde55e4d01abc38fd7ded4453
Федор <radiatorow@gmail.com>
2022-02-21 18:32:16 +0300

    all_working

--- funny_videos/views.py
+++ funny_videos/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Video
 
 def index(request):
-    return HttpResponse('At this page will be some funny videos')
+    videos = Video.objects.all()
+    template = loader.get_template('funny_videos/video.html')
+    context = {'videos': videos}
+
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
