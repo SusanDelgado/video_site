7c44c0fb569167886a6b1830375af19ddf4bd7c8
Федор <radiatorow@gmail.com>
2022-02-22 18:23:56 +0300

    adde new_page

--- funny_videos/urls.py
+++ funny_videos/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
+from .views import add_new
 
 urlpatterns = [
-    path('add_new/', views.add_new, name = 'add_new'),
+    path('add_new/', add_new.as_view(), name = 'add_new'),
     path('',views.index, name = 'index'),
 ]
--- funny_videos/views.py
+++ funny_videos/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Video
+from django.urls import reverse_lazy
+from .forms import PostForm
+from django.views.generic import ListView, CreateView
 
 def index(request):
     videos = Video.objects.all()
@@ -10,5 +13,11 @@ def index(request):
 
     return HttpResponse(template.render(context, request))
 
-def add_new(request):
-    return HttpResponse('Adding_video')
+class add_new(CreateView):
+    model = Video  
+    form_class = PostForm
+    template_name = 'funny_videos/add_new.html'
+    success_url = reverse_lazy('index')
+
+
+
